e9fed7da9cf9d68a2c4703315fce137985dc0509
bespontoff <bespontoff@gmail.com>
2020-03-02 23:51:23 +0300

    update FT. now him use unittest

--- functional_tests.py
+++ functional_tests.py
@@ -1,5 +1,18 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://127.0.0.1:8000')
-assert 'Django' in browser.title
+
+class UserViewTests(unittest.TestCase):
+    def setUp(self) -> None:
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self) -> None:
+        self.browser.quit()
+
+    def test_view_home_page_with_todo_list(self):
+        self.browser.get('http://127.0.0.1:8000')
+        assert 'TODO' in self.browser.title
+
+
+if __name__ == '__main__':
+    unittest.main()
