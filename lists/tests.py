1ef004343e5d3fa6d131cfa73a4297c7f553e34b
bespontoff <bespontoff@gmail.com>
2020-03-03 23:50:51 +0300

    fist test and first view home_page

--- lists/tests.py
+++ lists/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
 
 # Create your tests here.
+from django.urls import resolve
+from.views import home_page
+
+
+class HomePageTest(TestCase):
+
+    def root_url_resolve_to_home_page_view(self):
+        found = resolve('/')
+        self.assertEqual(found.func, home_page)
