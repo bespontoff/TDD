1dcfbddd189a32cee64f0cb247594aca4c3fefeb
bespontoff <bespontoff@gmail.com>
2020-03-05 09:26:03 +0300

    add new test for post request

--- lists/tests.py
+++ lists/tests.py
@@ -11,3 +11,7 @@ class HomePageTest(TestCase):
     def test_uses_home_template(self):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'lists/home.html')
+
+    def test_can_save_a_POST_request(self):
+        response = self.client.post('/', data={'item_text': 'New item list'})
+        self.assertIn('New item list', response.content.decode())
