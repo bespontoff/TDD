b84b42d4abc2f5e8a40a4721d7ea3534de510a58
denisov <max.kvando@gmail.com>
2020-03-11 13:24:23 +0300

    refactor FT for wait items in inf loop with timelimit

--- tests_functional/tests.py
+++ tests_functional/tests.py
@@ -2,20 +2,32 @@ import time
 from selenium import webdriver
 from django.test import LiveServerTestCase
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import WebDriverException
 
 
 class UserViewTests(LiveServerTestCase):
+    MAX_WAIT = 10
+
     def setUp(self) -> None:
         self.browser = webdriver.Firefox()
 
     def tearDown(self) -> None:
         self.browser.quit()
 
-    def check_item_in_list(self, item):
-        todo_list = self.browser.find_element_by_id('todo_list')
-        todo_items = todo_list.find_elements_by_tag_name('li')
-        self.assertIn(item, [item.text for item in todo_items],
-                      f'Not found added text in TODO list: {todo_list.text}')
+    def wait_item_in_list(self, item):
+        start_time = time.time()
+
+        while True:
+            try:
+                todo_list = self.browser.find_element_by_id('todo_list')
+                todo_items = todo_list.find_elements_by_tag_name('li')
+                self.assertIn(item, [item.text for item in todo_items],
+                              f'Not found added text in TODO list: {todo_list.text}')
+                return
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > self.MAX_WAIT:
+                    raise e
+                time.sleep(.5)
 
     def test_view_home_page_with_todo_list_and_retrieve_it_later(self):
         self.browser.get(self.live_server_url)
@@ -34,9 +46,8 @@ class UserViewTests(LiveServerTestCase):
         # Когда она нажимает enter, страница обновляется, и теперь страница
         # содержит "1: Купить павлиньи перья" в качестве элемента списка
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
 
-        self.check_item_in_list('Купить павлиньи перья')
+        self.wait_item_in_list('Купить павлиньи перья')
 
         # Текстовое поле по-прежнему приглашает ее добавить еще один элемент.
         # Она вводит "Сделать мушку из павлиньих перьев"
@@ -44,10 +55,9 @@ class UserViewTests(LiveServerTestCase):
         inputbox = self.browser.find_element_by_id('add_item')
         inputbox.send_keys('Сделать мушку из павлиньих перьев')
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
         # Страница снова обновляется, и теперь показывает оба элемента ее списка
-        self.check_item_in_list('Купить павлиньи перья')
-        self.check_item_in_list('Сделать мушку из павлиньих перьев')
+        self.wait_item_in_list('Купить павлиньи перья')
+        self.wait_item_in_list('Сделать мушку из павлиньих перьев')
 
         # Эдит интересно, запомнит ли сайт ее список. Далее она видит, что
         # сайт сгенерировал для нее уникальный URL-адрес – об этом
