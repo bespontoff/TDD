e2845743952b3e6e1a6c64a3cea6c15a4bdc2a74
bespontoff <bespontoff@gmail.com>
2020-03-23 19:46:52 +0300

    FT refactor

--- tests_functional/tests.py
+++ tests_functional/tests.py
@@ -99,3 +99,9 @@ class UserViewTests(LiveServerTestCase):
         # Она посещает этот URL-адрес – ее список по-прежнему там.
         # Удовлетворенная, она снова ложится спать
         # self.fail('Дописать тест')
+
+    def test_layout_and_styling(self):
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
+        inputbox = self.browser.find_element_by_id('add_item')
+        self.assertAlmostEqual(inputbox.location['x'] + inputbox.size['width']/2, 512, delta=10)
